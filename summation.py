778619b303a314abe2568af3d42173d89c08f98f
Trevor Swarm <tswarm@lccc.wy.edu>
2024-11-27 15:13:12 -0700

    Add checkTotal and checkString variables to summation.py and create a test for checkString

--- summation.py
+++ summation.py
@@ -1,2 +1,9 @@
 def add_one(n):
     return n+1
+
+checkTotal = 0
+checkString = "Not Run"
+
+
+checkTotal = add_one(1)
+checkString = "Run"
---
+++ summation_test_2.py
@@ -0,0 +1,5 @@
+import pytest, summation
+from summation import add_one
+
+def test_run():
+    assert summation.checkString == "Runer"

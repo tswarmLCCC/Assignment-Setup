7672b3f008de734472c7c56e2cf0250578cd3597
Trevor Swarm <tswarm@lccc.wy.edu>
2024-11-30 15:51:19 -0700

    Add Make_Solution_root.py and tests for user input/output functionality

---
+++ tests/Make_Test_1.py
@@ -0,0 +1,43 @@
+# DO NOT MODIFY THE CODE IN THIS FILE
+
+# Tests for Problem 1
+
+import sys
+sys.path.append("..")
+
+import os.path
+import sys
+from  Make_Solution_root import main
+import Make_Solution_root
+from tud_tests import *
+
+def test_problem_1():
+
+    try:
+        exists = os.path.exists("../Make_Solution_root.py")
+        assert exists == True
+    except:
+        sys.exit()
+
+    set_keyboard_input([5])
+    main()
+    output = get_display_output()
+   #print(output)
+
+    assert output == ["Enter a number between 0 and 23: ", "Too early to get up"]
+
+    assert output[0].lower() == "enter a number between 0 and 23: "
+
+    assert Make_Solution_root.number == 5
+    '''
+    assert output == [
+            "Simple Pay Program\n",
+            "How many hours did you work: ",
+            "\nPay Stub",
+            "\t\tHours: 5",
+            "\t\tRate: $25.00",
+            "\t\tPay: $125.00",
+            "\t\tTax: $15.62",
+            "\t\tNet Pay: $109.38"   
+    ]
+    '''
